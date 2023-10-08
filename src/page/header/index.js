49f88f5f8f53398308c5628b4b54ea8b51f2d618
PhongVu0705 <pvu0705@gmail.com>
2023-10-08 22:45:09 +0700

    update1

---
+++ src/page/header/index.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+
+function PageHeader() {
+  return (
+    <div>
+      <Container
+        maxWidth="false"
+        sx={{
+          bgcolor: "DarkSalmon",
+        }}
+      >
+        
+      </Container>
+    </div>
+  );
+}
+
+export default PageHeader;
